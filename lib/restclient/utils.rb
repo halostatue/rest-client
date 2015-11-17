24c4c4366fe1ab82c2407f5d6cc0dbc597485569
Andy Brody <git@abrody.com>
2015-11-17 01:58:30 -0800

    WIP

--- lib/restclient/utils.rb
+++ lib/restclient/utils.rb
@@ -11,10 +11,14 @@ module RestClient
     # Strings will effectively end up using `Encoding.default_external` when
     # this method returns nil.
     #
-    # @param headers [Hash]
+    # @param headers [Hash<Symbol,String>]
     #
     # @return [String, nil] encoding Return the string encoding or nil if no
     #   header is found.
+    #
+    # @example
+    #   >> get_encoding_from_headers({:content_type => 'text/plain; charset=UTF-8'})
+    #   => "UTF-8"
     #
     def self.get_encoding_from_headers(headers)
       type_header = headers[:content_type]
@@ -54,7 +58,7 @@ module RestClient
       nil
     end
 
-    # Parse a Content-type like header.
+    # Parse a Content-Type like header.
     #
     # Return the main content-type and a hash of options.
     #
@@ -100,7 +104,7 @@ module RestClient
     # simple flat hashes, you may want to use `URI.encode_www_form` instead,
     # which implements HTML5-compliant URL encoded form data.
     #
-    # @param [Object] object The object to serialize
+    # @param [Hash,ParamsArray] object The object to serialize
     # @param [String] parent_key The parent hash key of this object
     #
     # @return [String] A string appropriate for use as an HTTP query string
@@ -118,6 +122,10 @@ module RestClient
     #   omitted entirely from the output. Rather than disappearing, they will
     #   appear to be nil instead.
     #
+    # It's most common to pass a Hash as the object to serialize, but you can
+    # also use a ParamsArray if you want to be able to pass the same key with
+    # multiple values and not use the rack/rails array convention.
+    #
     # @since 2.0.0
     #
     # @example Simple hashes
@@ -132,22 +140,41 @@ module RestClient
     #   >> encode_query_string({outer: {foo: 123, bar: 456}})
     #   => 'outer[foo]=123&outer[bar]=456'
     #
+    # @example Deeply nesting
+    #   >> encode_query_string({coords: [{x: 1, y: 0}, {x: 2}, {x: 3}]})
+    #   => 'coords[][x]=1&coords[][y]=0&coords[][x]=2&coords[][x]=3'
+    #
     # @example Null and empty values
-    #   >> encode_query-string({string: '', empty: nil, list: [], hash: {}})
+    #   >> encode_query_string({string: '', empty: nil, list: [], hash: {}})
     #   => 'string=&empty&list&hash'
     #
     # @example Nested nulls
-    #   >> encode_query-string({foo: {string: '', empty: nil}})
+    #   >> encode_query_string({foo: {string: '', empty: nil}})
     #   => 'foo[string]=&foo[empty]'
     #
-    def self.encode_query_string(object, parent_key=nil)
-      if !parent_key && !object.is_a?(Hash)
-        raise ArgumentError.new('top level query param must be a Hash, got: ' +
-                                object.inspect)
+    # @example Multiple fields with the same name using ParamsArray
+    #   >> encode_query_string(RestClient::ParamsArray.new([[:foo, 1], [:foo, 2], [:foo, 3]])
+    #   => 'foo=1&foo=2&foo=3'
+    #
+    # @example Nested ParamsArray
+    #   >> encode_query_string({foo: RestClient::ParamsArray.new([[:a, 1], [:a, 2]])})
+    #   => 'foo[a]=1&foo[a]=2'
+    #
+    #   >> encode_query_string(RestClient::ParamsArray.new([[:foo, {a: 1}], [:foo, {a: 2}]]))
+    #   => 'foo[a]=1&foo[a]=2'
+    #
+    def self.encode_query_string2(object, parent_key=nil)
+      if !parent_key
+        case object
+        when Hash, ParamsArray
+        else
+          raise ArgumentError.new('top level query param must be a Hash or ' +
+                                  'ParamsArray, got: ' + object.inspect)
+        end
       end
 
       case object
-      when Hash
+      when Hash, ParamsArray
         if object.empty?
           if parent_key
             return encode_query_string(nil, parent_key)
@@ -175,6 +202,60 @@ module RestClient
       else
         "#{parent_key}=#{escape(object.to_s)}"
       end
+    end
+
+    def self.encode_query_string(object)
+      flatten_params(object, true).map {|k, v| v.nil? ? k : "#{k}=#{v}" }.join('&')
+    end
+
+    # Transform deeply nested param containers into a flat array of [key,
+    # value] pairs.
+    #
+    # @example
+    #   >> flatten_params({key1: {key2: 123}})
+    #   => [["key1[key2]", 123]]
+    #
+    # @example
+    #   >> flatten_params({key1: {key2: 123, arr: [1,2,3]}})
+    #   => [["key1[key2]", 123], ["key1[arr][]", 1], ["key1[arr][]", 2], ["key1[arr][]", 3]]
+    #
+    # @param object [Hash, ParamsArray] The container to flatten
+    # @param uri_escape [Boolean] Whether to URI escape keys and values
+    # @param parent_key [String] Should not be passed (used for recursion)
+    #
+    def self.flatten_params(object, uri_escape=false, parent_key=nil)
+      unless object.is_a?(Hash) || object.is_a?(ParamsArray) ||
+             (parent_key && object.is_a?(Array))
+        raise ArgumentError.new('expected Hash or ParamsArray, got: ' + object.inspect)
+      end
+
+      # transform empty collections into nil, where possible
+      if object.empty? && parent_key
+        return [[parent_key, nil]]
+      end
+
+      # This is essentially .map(), but we need to do += for nested containers
+      object.reduce([]) { |result, item|
+        if object.is_a?(Array)
+          # item is already the value
+          k = nil
+          v = item
+        else
+          # item is a key, value pair
+          k, v = item
+          k = escape(k.to_s) if uri_escape
+        end
+
+        processed_key = parent_key ? "#{parent_key}[#{k}]" : k
+
+        case v
+        when Array, Hash, ParamsArray
+          result.concat flatten_params(v, uri_escape, processed_key)
+        else
+          v = escape(v.to_s) if uri_escape && v
+          result << [processed_key, v]
+        end
+      }
     end
 
     # Encode string for safe transport by URI or form encoding. This uses a CGI
